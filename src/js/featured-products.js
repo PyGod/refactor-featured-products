940a1b08893b66b7591f9966e7ce55f76ec17b88
PyGod <pythonloveyou@gmail.com>
2025-11-11 18:56:50 +0200

    Create render method to get product collection from Shopify API and start to specify addEventListener for Add to cart button

--- src/js/featured-products.js
+++ src/js/featured-products.js
@@ -10,4 +10,26 @@ class FeaturedProducts extends HTMLElement {
     this.container = this.querySelector('.featured-products__list');
     this.addEventListener();
   }
+
+  async render() {
+    const response = await fetch(
+      `/collections/${this.collectionHandle}?view=featured-products`
+    );
+    const html = await response.text();
+    this.innerHTML = `
+      <section class="featured-products">
+        <h2 class="featured-products__title">Featured Products</h2>
+        <div class="featured-products__list">${html}</div>
+      </section>
+    `;
+  }
+
+  addEventListener() {
+    this.addEventListener('click', async (event) => {
+      if (event.target.matches('.featured-products__add-to-cart')) {
+        const form = event.target.closest('form');
+        const formData = new FormData(form);
+      }
+    });
+  }
 }
